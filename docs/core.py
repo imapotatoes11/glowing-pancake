3c83a81ca50e7f06a08a1093165d39596df9f92b
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-02 10:57:29 -0500

    stuff?

---
+++ docs/core.py
@@ -0,0 +1,6 @@
+import webbrowser as wb
+def inquiry(inp):
+    wb.open_new(f"https://www.google.com/search?q={inp}+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8")
+
+#inquiry("canada")
+wb.open_new("imapotatoes11.github.io")
