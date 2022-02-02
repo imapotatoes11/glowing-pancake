e76e55e17fe8fcd7385db5b45d838641a3ac436d
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-02 13:14:59 -0500

    fsdbhsfdhngsdhsdgh

---
+++ docs/core.js
@@ -0,0 +1,11 @@
+
+//def inquiry(inp):
+//return (f"https://www.google.com/search?q={inp}+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8")
+
+//print(inquiry("canada"))
+
+function inquiry(inp) {
+    document.getElementById("hi").innerHTML="https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8";
+    return "https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8";
+}
+console.log(inquiry("canada"));
--- docs/core.py
+++ docs/core.py
@@ -1,6 +1,4 @@
-import webbrowser as wb
-chromePath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-wb.get(chromePath)
+
 def inquiry(inp):
     return (f"https://www.google.com/search?q={inp}+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8")
 
