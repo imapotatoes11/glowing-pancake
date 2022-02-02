71c2ffb02821d711e25bf023dec9908c3d8db60f
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-02 11:03:08 -0500

    ragragfr

--- docs/core.py
+++ docs/core.py
@@ -1,6 +1,7 @@
 import webbrowser as wb
+chromePath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+wb.get(chromePath)
 def inquiry(inp):
-    wb.open_new(f"https://www.google.com/search?q={inp}+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8")
+    return (f"https://www.google.com/search?q={inp}+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8")
 
-#inquiry("canada")
-wb.open_new("imapotatoes11.github.io")
+print(inquiry("canada"))
