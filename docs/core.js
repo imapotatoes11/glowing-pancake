0994f6539d59fea740f1d763051991d87ba21c8c
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-02 13:18:21 -0500

    dsfasgdasg

--- docs/core.js
+++ docs/core.js
@@ -4,7 +4,8 @@
 
 //print(inquiry("canada"))
 
-function inquiry(inp) {
+function inquiry() {
+    let inp = document.getElementById("bob").value;
     document.getElementById("hi").innerHTML="https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8";
     return "https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8";
 }
