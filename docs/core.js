af56e521b6e3907f5598188191327278405814c8
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-02 13:24:40 -0500

    fsgafh

--- docs/core.js
+++ docs/core.js
@@ -7,7 +7,7 @@
 function inquiry() {
     let inp = document.getElementById("bob").value;
     let anchor = document.createElement("a")
-    let text = document.createTextNode("AHHHHHHHHHHHHHHHHHHHHHH")
+    let text = document.createTextNode("yesyes")
     anchor.href = "https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8"
     anchor.appendChild(text)
     document.body.appendChild(anchor)
