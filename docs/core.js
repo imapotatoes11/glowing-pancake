da28bd1c976e704759e94ab689e3b2e783706fe0
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-03 10:42:43 -0500

    fgadhaef

--- docs/core.js
+++ docs/core.js
@@ -1,3 +1,21 @@
+function wikipedia(animal) {
+	array = []
+	for (let i = 0; i < animal.length; i++) {
+  	array.push(animal[i])	
+  }
+  for (let i = 0; i < array.length; i++) {
+  	if (array[i] == " ") {
+    	array[i] = "_"
+    }
+  }
+  let link = "https://en.wikipedia.org/wiki/" + array.join("").toLowerCase()
+	return link
+}
+
+
+
+
+
 let anchor = document.createElement("a")
 let text = document.createTextNode("Link to view more information")
 anchor.appendChild(text)
@@ -5,6 +23,7 @@ let counter = 0;
 
 //animals = ["google", "https://www.google.com/", "youtube", "https://www.youtube.com/"]
 animals = ["ok", "https://en.wikipedia.org/wiki/2", "ok alex", "https://en.wikipedia.org/wiki/4"]
+animals=["alabama cave shrimp","https://www.wikipedia.com/wiki/Alabama_cave_shrimp","Alabama","https://www.wikipedia.com/wiki/Alabama"]
 
 function run(count) {
 
