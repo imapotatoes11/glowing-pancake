9c8b7903188776a47644b8137c788fe115ddc566
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-03 18:36:55 -0500

    canada and us i think

--- convertformat.py
+++ convertformat.py
@@ -2,11 +2,14 @@ Bobs=[]
 with open('converttest.txt','r') as file:
     for i in file.readlines():
         Bobs.append((i.removesuffix('\n')).removesuffix('Â\xa0Â\xa0'))
+        print('doing '+(i.removesuffix('\n')).removesuffix('Â\xa0Â\xa0'))
 
 for i in range(len(Bobs)):
     Bobs[i]='\''+Bobs[i]
     Bobs[i]=Bobs[i]+'\', '
+    print('converting '+Bobs[i]+'... ')
 
 for j in Bobs:
     with open('convertoutput.txt','a') as file:
         file.write(j)
+        print('writing '+j+'.....')
