ab6e38a451477236995fb46423a11135c62c67b2
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-03 11:23:33 -0500

    yes?

---
+++ convertformat.py
@@ -0,0 +1,12 @@
+Bobs=[]
+with open('converttest.txt','r') as file:
+    for i in file.readlines():
+        Bobs.append((i.removesuffix('\n')).removesuffix('Â\xa0Â\xa0'))
+
+for i in range(len(Bobs)):
+    Bobs[i]='\''+Bobs[i]
+    Bobs[i]=Bobs[i]+'\',\n'
+
+for j in Bobs:
+    with open('convertoutput.txt','a') as file:
+        file.write(j)
