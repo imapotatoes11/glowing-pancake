047d1f00c25ba92aa2c90e7684ab22bc3e44bf01
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-02-02 21:17:30 -0500

    fdbbvzcb

--- docs/core.js
+++ docs/core.js
@@ -1,17 +1,40 @@
+let anchor = document.createElement("a")
+let text = document.createTextNode("Link to view more information")
+anchor.appendChild(text)
+let counter = 0;
 
-//def inquiry(inp):
-//return (f"https://www.google.com/search?q={inp}+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8")
+animals = ["google", "https://www.google.com/", "youtube", "https://www.youtube.com/"]
 
-//print(inquiry("canada"))
+function run(count) {
 
-function inquiry() {
-    let inp = document.getElementById("bob").value;
-    let anchor = document.createElement("a")
-    let text = document.createTextNode("yesyes")
-    anchor.href = "https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8"
-    anchor.appendChild(text)
-    document.body.appendChild(anchor)
-    document.getElementById("hi").innerHTML="https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8";
-    return "https://www.google.com/search?q="+inp+"+endangered+animals&rlz=1C1CHBF_enCA970CA970&oq=canada+endangered+animals&aqs=chrome..69i57j0i390.3725j0j7&sourceid=chrome&ie=UTF-8";
+  if (count == animals.length) {
+
+    return 0
+
+  }
+  if (count % 2 == 0) {
+    document.getElementById("hello").innerHTML += animals[count] + " "
+  } else if (count % 2 != 0) {
+    anchor.href = animals[count]
+    document.getElementById('hello').appendChild(anchor)
+    document.getElementById('hello').innerHTML += "<br>"
+  }
+    run(count+1)
+
 }
-console.log(inquiry("canada"));
+
+function counting() {
+
+  counter++
+  if (counter == 1) {
+
+    run(0)
+
+  } else if (counter != 1) {
+
+    document.getElementById('hello') = " "
+    run(0)
+
+  }
+
+}
